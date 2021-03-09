97fd793e67baba78937c04e4c4977beac8f313c6
Emerald Mclaniyi <emclaniyi@gmail.com>
2021-03-09 23:44:50 +0100

    started working fuzzy matcher class

--- DataMatcher.py
+++ DataMatcher.py
@@ -9,10 +9,12 @@ class DataMatcher:
     			data_list (list of data per row) a list of floats extracted from the data file
     """
 
-    def __int__(self):
-        self.data = []
 
-    def read_data_file(self, data):
+    def __int__(self):
+        self.data_one = []
+        self.data_two = []
+
+    def read_data_one(self, data_one):
         """Function to read in data from a txt file. The txt file should have
         one number (float) per line. The numbers are stored in the data attribute.
 
@@ -23,6 +25,21 @@ class DataMatcher:
             None
 
         """
-        pd.read_csv(data)
-        data_list = []
-        self.data = data_list
+        data_one = pd.read_csv(data_one)
+        self.data_one = data_one
+
+
+    def read_data_two(self, data_two):
+        """Function to read in data from a txt file. The txt file should have
+        one number (float) per line. The numbers are stored in the data attribute.
+
+        Args:
+            data (csv): name of a file to read from
+
+        Returns:
+            None
+
+        """
+
+        data_two = pd.read_csv(data_two)
+        self.data_two = data_two
---
+++ DataMatcherFuzzy.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import fuzzymatcher
+from .DataMatcher import DataMatcher
+
+
+class DataMatcherFuzzy(DataMatcher):
+
+    def __int__(self):
+        self.left_on = []
+        self.right_on = []
+
+    def to_match(self, left_on, right_on):
+        self.left_on = left_on
+        self.right_on = right_on
+
+        matched_results = fuzzymatcher.fuzzy_left_join(DataMatcher.read_data_one(self, data_one=None),
+                                                       DataMatcher.read_data_two(self, data_two=None),
+                                                       left_on,
+                                                       right_on,
+                                                       left_id_col='Account_Num',
+                                                       right_id_col='Provider_Num')
