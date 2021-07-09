1e5a848179793bc23aacb4baff61c1baa1054567
Emerald Mclaniyi <emclaniyi@gmail.com>
2021-07-09 13:56:22 +0100

    ocr-api

---
+++ ocr_core.py
@@ -0,0 +1,37 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+import cv2
+
+
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(
+        filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
+
+
+# print(ocr_core('images/staff-id-card.jpg'))
+
+# Create the voice_text variable to store the data.
+
+voice_text = ""
+
+# Pre-processing the data
+
+for i in ocr_core('images/staff-id-card.jpg').split():
+    voice_text += i + ' '
+
+voice_text = voice_text[:-1]
+voice_text
+
+from gtts import gTTS
+from playsound import playsound
+
+tts = gTTS(voice_text)
+tts.save("test.mp3")
+playsound("test.mp3")
---
+++ ocr_server.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template, request
+
+# import our OCR function
+from ocr_core import ocr_core
+
+# define a folder to store and later serve the images
+UPLOAD_FOLDER = '/static/uploads/'
+
+# allow files of a specific type
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+
+# function to check the file extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# route and function to handle the home page
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
